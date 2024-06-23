2d9504df977b68c5a9f3320d59e6be3cd49c3db9
afrihar <afrihar@gmail.com>
2024-06-23 21:54:45 +0700

    Initial commit: Add .gitignore file

---
+++ src/main/java/com/example/elksblogging/ElkSbLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.example.elksblogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElkSbLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElkSbLoggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/elksblogging/ElkSbLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elksblogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkSbLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
